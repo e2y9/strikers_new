0f3bf691ab0d9e2c27a20260d0ef2eee8c56f125
Emmet Young <emmetkyoung@gmail.com>
2020-02-05 23:11:20 +0000

    working on test log

--- src/commandline/GameLogic.java
+++ src/commandline/GameLogic.java
@@ -10,7 +10,7 @@ public class GameLogic {
 	private Players playersList;
 	private DeckOfCards allCards;
 	private Player winnerOfRound;
-	private DeckOfCards commonPile;
+	private DeckOfCards drawPile;
 	private int totalNumberOfDraws = 0;
 	private static int gameId = 0;
 	
@@ -260,7 +260,20 @@ public class GameLogic {
 		}
 	}
 	
-	public DeckOfCards getCommonPile() {
-		return commonPile;
+	public DeckOfCards getDrawPile() {
+		return drawPile;
+	}
+	
+	public void addToDrawPile(Card card) {
+		this.drawPile.addCard(card);
+	}
+	
+	public void removeFromDrawPile(Card card) {
+		for (Card c : drawPile.getDeck()) {
+			if (card == c) {
+				drawPile.getDeck().remove(c);
+				break;
+			}
+		}
 	}
 }
