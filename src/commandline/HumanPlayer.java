5481eec88a7fe84e17a241e3a7c2ea105536ab25
Emmet Young <emmetkyoung@gmail.com>
2020-02-07 00:44:39 +0000

    CLI game loop working

--- src/commandline/HumanPlayer.java
+++ src/commandline/HumanPlayer.java
@@ -19,26 +19,24 @@ public class HumanPlayer implements Player {
 	  }
 	  
 
-
-
 	@Override
 	  public int chooseCategory() {
 		  int catNum = 0;
 		  try {
-			  System.out.printf("%n%nWhat category would you like to choose?%n");
-			  System.out.println("0 - Intelligence");
-			  System.out.println("1 - Speed");
-			  System.out.println("2 - Strength");
-			  System.out.println("3 - Agility");
-			  System.out.println("4 - Combat");
-//			  System.out.println("5 - Quit");
+			  System.out.println("\n\n- - - - - - - - - -\n\nWhich category would you like to choose?\n");
+			  System.out.println("1 - Intelligence");
+			  System.out.println("2 - Speed");
+			  System.out.println("3 - Strength");
+			  System.out.println("4 - Agility");
+			  System.out.println("5 - Combat");
 			  System.out.println("");
 			  Scanner s = new Scanner(System.in);
 			  String category = s.nextLine();
 			  catNum = Integer.parseInt(category);
 			  // array starts at 0, above list starts at 1, so -1
-		  } catch (Exception e) {
-			  System.out.println("Please enter a number between 0 and 4.");
+			  catNum--;
+		  } catch (ArrayIndexOutOfBoundsException e) {
+			  System.out.println("Please enter a number between 1 and 5.");
 		  }
 		  
 		  return catNum;
